a10e502f66ee722528bb1f84c54919db622134e5
Stelian <steliannechita2013@gmail.com>
2020-01-10 12:39:58 +0000

    stelian_fareed_1

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -7,4 +7,27 @@ describe('Hotel', () => {
     it('new Hotel returns a Hotel',()=>{
         expect(new Hotel()).to.be.an.instanceOf(Hotel)
     })
+    it('should give the hotel a name',()=>{
+        expect(new Hotel('my hotel').name).to.equal('my hotel')
+    })
+    it('should give the hotel a city',()=>{
+        expect(new Hotel('london').name).to.equal('london')
+    })
+    it('should return an array from Hotel reviews',()=>{
+        expect(new Hotel('my Hotel','London').reviews).to.be.an('array')
+    })
+})
+describe('Hotel methods',()=>{
+    it('should return a number from reviewCount()',()=>{
+        expect(new Hotel('my Hotel','London').reviewCount()).to.be.a('number')
+    })
+    it('should return a number from rating()',()=>{
+        expect(new Hotel('my Hotel','London').rating()).to.be.a('number')
+    })
+    it('should return a string from ratingAsStars()',()=>{
+        expect(new Hotel('my Hotel','London').ratingAsStars()).to.be.a('string')
+    })
+    it('should return a number from urlSlug()',()=>{
+        expect(new Hotel('my Hotel','London').urlSlug()).to.be.a('string')
+    })
 })
