f7e5703f1bac6d7a5e910b94f54088313e0532ee
alexcasper <not@territory.cc>
2020-01-10 12:13:58 +0000

    tests for name and city

--- test/hotel.test.js
+++ test/hotel.test.js
@@ -4,7 +4,13 @@ const expect = chai.expect;
 var Hotel = require('../models/hotel')
 
 describe('Hotel', () => {
-    it('new Hotel returns a Hotel',()=>{
+    it('should return a Hotel from new Hotel',()=>{
         expect(new Hotel()).to.be.an.instanceOf(Hotel)
+    })
+    it('should give the Hotel a name', ()=>{
+        expect(new Hotel('my Hotel').name).to.equal('my Hotel')
+    })
+    it('should give the Hotel a city', ()=>{
+        expect(new Hotel('my Hotel','London').city).to.equal('London')
     })
 })
